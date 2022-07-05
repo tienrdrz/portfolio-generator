43a10abcb2e775cb8a277391a9028798a4663294
etienne <etiennerodriguez3@gmail.com>
2022-07-05 13:05:01 -0400

    create html file

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // This ...
+const generatePage = require('./src/page-template.js');
 
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    //Is the same as this ...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
